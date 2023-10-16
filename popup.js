d49abf0ca19c1c85a56d2da22910011796085946
Samir84753 <samir84753@gmail.com>
2023-10-16 14:36:15 +0545

    refactoring

--- popup.js
+++ popup.js
@@ -5,24 +5,26 @@ document.addEventListener('DOMContentLoaded', function () {
 
   copyTranscriptButton.addEventListener('click', copyTranscript);
   showTranscriptButton.addEventListener('click', showTranscript);
-  hideTranscriptButton.addEventListener('click',hideTranscript);
+  hideTranscriptButton.addEventListener('click', hideTranscript);
 
   showTranscriptButton.addEventListener('click', () => {
     copyTranscriptButton.style.display = 'block';
   });
 
   hideTranscriptButton.addEventListener('click', () => {
-    copyTranscriptButton.style.display = 'none'; 
+    copyTranscriptButton.style.display = 'none';
   });
 });
+
 function hideMessage(messageElement, delay) {
   setTimeout(() => {
-    messageElement.textContent = ''; 
+    messageElement.textContent = '';
   }, delay);
 }
+
 function showAndHideMessages(messageElement, message, delay) {
-  messageElement.textContent = message; // Set the message
-  hideMessage(messageElement, delay); // Hide the message after the specified delay
+  messageElement.textContent = message;
+  hideMessage(messageElement, delay);
 }
 
 function copyTranscript() {
@@ -38,22 +40,24 @@ function copyTranscript() {
           const transcriptText = Array.from(transcriptElements).map(element => element.innerText).join('\n');
           return transcriptText;
         }
+        return null;
       },
     }, function (result) {
-      const transcriptText = result[0].result;
-      
-      // Copy the transcriptText to the clipboard
-      const textarea = document.createElement('textarea');
-      textarea.value = transcriptText;
-      document.body.appendChild(textarea);
-      textarea.select();
-      document.execCommand('copy');
-      document.body.removeChild(textarea);
+      const transcriptText = result[0]?.result;
 
-      // Display the copied text message in the popup
-      const messageElement = document.getElementById('copyMessage');
-      showAndHideMessages(messageElement, 'Transcript copied to clipboard.', 1500);
+      if (transcriptText) {
+        // Copy the transcriptText to the clipboard
+        const textarea = document.createElement('textarea');
+        textarea.value = transcriptText;
+        document.body.appendChild(textarea);
+        textarea.select();
+        document.execCommand('copy');
+        document.body.removeChild(textarea);
 
+        // Display the copied text message in the popup
+        const messageElement = document.getElementById('copyMessage');
+        showAndHideMessages(messageElement, 'Transcript copied to clipboard.', 1500);
+      }
     });
   });
 }
@@ -70,19 +74,15 @@ function showTranscript() {
           transcriptPanel.setAttribute('visibility', 'ENGAGEMENT_PANEL_VISIBILITY_EXPANDED');
           return true;
         }
-        else{
-          return false;
-        }
+        return false;
       },
     }, function (result) {
       const messageElement = document.getElementById('showTranscriptMessage');
-      const message = result[0].result == true?'Transcript is now visible.':'Transcript is not available for this video.';
-      showAndHideMessages(messageElement,message, 1500);
+      const message = result[0].result ? 'Transcript is now visible.' : 'Transcript is not available for this video.';
+      showAndHideMessages(messageElement, message, 1500);
 
-      //hide copy button if no transcript available.
-      if (result[0].result==false){
-        document.getElementById('copyTranscript').style.display = 'none';
-      }
+      // Hide copy button if no transcript available
+      document.getElementById('copyTranscript').style.display = result[0].result ? 'block' : 'none';
     });
   });
 }
@@ -99,16 +99,14 @@ function hideTranscript() {
           transcriptPanel.setAttribute('visibility', 'ENGAGEMENT_PANEL_VISIBILITY_HIDDEN');
           return true;
         }
-        else {
-          return false;
-        }
+        return false;
       },
     }, function (result) {
       if (result[0].result) {
         // Display the message in the popup
         const messageElement = document.getElementById('hideTranscriptMessage');
         showAndHideMessages(messageElement, 'Transcript is now hidden.', 1500);
-      } 
+      }
     });
   });
 }
